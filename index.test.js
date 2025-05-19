4ad9ca68dd7874998acb146b83292c341cd44a5f
Emmanuel Luis <Emmanuel.luis@verizon.com>
2025-05-19 11:45:07 -0500

    Implemented app.js and index.test.js

--- index.test.js
+++ index.test.js
@@ -2,16 +2,34 @@
 const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
-
-const request = require("supertest")
 const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
 const {seedMusician} = require("./seedData");
+const request = require("supertest");
 
 
 describe('./musicians endpoint', () => {
-    // Write your tests here
+    test("Verify GET /musicians", async () => {
+        const response = await request(app).get("/musicians");
+        const responseJson = JSON.parse(response.text);
+
+        expect(response.statusCode).toBe(200);
+        expect(responseJson).toMatchObject([
+            {
+            name: 'Mick Jagger',
+            instrument: 'Voice'
+            },
+            {
+            name: 'Drake',
+            instrument: 'Voice',
+            },
+            {
+            name: 'Jimi Hendrix',
+            instrument: 'Guitar'
+            }
+        ])
+    })
     
     
 
