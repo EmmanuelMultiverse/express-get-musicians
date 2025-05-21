cf9d9fcc3c06ded09d7f9c9d6058845bf766fb9e
--local <--local>
2025-05-21 09:21:55 -0500

    Added bands.js to /routes & implemented /GET routes & implemented bandsRouter in app.js & removed predefined routes

---
+++ routes/bands.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { Band } = require("../models");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    try {
+        const bands = await Band.findAll();
+
+        if (bands) {
+            res.status(200).json(bands);
+        } else {
+            res.status(400).send(`Could not find bands`);
+        }
+
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try {
+
+        const band = await Band.findByPk(req.params.id);
+
+        if (band) {
+            res.status(200).json(band);
+        } else {
+            res.status(400).send(`Could not find  ${req.params.id}`);
+        }
+
+    } catch (err) {
+        next(err);
+    }
+})
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -3,26 +3,13 @@ const app = express();
 const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 const musiciansRouter = require("../routes/musicians");
+const bandsRouter = require("../routes/bands");
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.use("/musicians", musiciansRouter);
-
-app.get("/bands", async (req, res, next) => {
-    try {
-        const bands = await Band.findAll();
-
-        if (bands) {
-            res.status(200).json(bands);
-        } else {
-            res.status(400).send(`Could not find bands`);
-        }
-    } catch (err) {
-        next(err);
-    }
-
-})
+app.use("/bands", bandsRouter);
 
 module.exports = app;
